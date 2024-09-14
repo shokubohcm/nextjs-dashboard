513ce05e4b6dd0b3e935b0276a1224d8801a5f68
久保田 翔 <sho.kubota@kubotanoMacBook-Air.local>
2024-09-14 11:24:00 +0900

    merge delete-auth-feature

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,7 +1,5 @@
 'use server';
 
-import { signIn } from '@/auth';
-import { AuthError } from 'next-auth';
 import { z } from 'zod';
 import { sql } from '@vercel/postgres'; 
 import { revalidatePath } from 'next/cache';
@@ -112,21 +110,3 @@ export async function deleteInvoice(id: string) {
   }
 }
 
-export async function authenticate(
-  prevState: string | undefined,
-  formData: FormData,
-) {
-  try {
-    await signIn('credentials', formData);
-  } catch (error) {
-    if (error instanceof AuthError) {
-      switch (error.type) {
-        case 'CredentialsSignin':
-          return 'Invalid credentials.';
-        default:
-          return 'Something went wrong.';
-      }
-    }
-    throw error;
-  }
-}
