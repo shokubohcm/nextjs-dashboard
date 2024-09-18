a62254875f719b5033b3bc88b8eb54d0558052de
久保田 翔 <sho.kubota@kubotanoMacBook-Air.local>
2024-09-18 09:00:14 +0900

    zasetsu

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,46 @@
-import Link from 'next/link';
+// import Link from 'next/link';
+import type {NextPage} from "next";
+import styles from "../styles/Home.module.css";
+import React, {useState} from "react";
+import { TextInput, Button } from "@mantine/core";
 
-export default function Page() {
-  return (
-    <main className="flex min-h-screen flex-col p-6">
-      <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
-            <Link href="/dashboard" className="text-blue-500 font-bold text-xl">
-              Go to Dashboard :)
-            </Link>
-        </div>
+const Home: NextPage = () => {
+  const [value, setValue] = useState("");
+  const [label, setLabel] = useState<"blue"|"orange">("blue");
+
+  const onChangeValue = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+  };
+
+  const toggle = () => {
+    if (label === "blue") {
+      setLabel("orange");
+    } else {
+      setLabel("blue");
+    }
+  };
+
+  return(
+    <main className={styles.main}>
+      <h1 className={styles.title}>Mantine hooks</h1>
+      <TextInput style={{margin: "1rem"}} value={value} onChange={onChangeValue} />
+      <Button color={label} onClick={() => toggle()}>
+        {label}
+      </Button>
     </main>
   );
-}
+};
+
+export default Home;
+
+// export default function Page() {
+//   return (
+//     <main className="flex min-h-screen flex-col p-6">
+//       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
+//             <Link href="/dashboard" className="text-blue-500 font-bold text-xl">
+//               Go to Dashboard :)
+//             </Link>
+//         </div>
+//     </main>
+//   );
+// }
